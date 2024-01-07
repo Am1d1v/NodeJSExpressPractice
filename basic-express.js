c9efe4fdfe2e038d49e77e18f768f6ac27eb1767
Am1d1v <dimadiman1adima@gmail.com>
2024-01-07 18:58:42 +0300

    .

--- basic-express.js
+++ basic-express.js
@@ -1,12 +1,24 @@
 
 
 const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
 
 
-const server = http.createServer((req, res) => {
-    res.end('Response from the server');
-});
-server.listen(3000, () => {
-    console.log(`Server was started on Port: 3000`);
+app.get('/', (req, res) => {
+    res.send('Express Response');
 })
+
+app.listen(3000, () => {
+    console.log(`Server was started on Port: 3000`);
+});
+
+
+// const server = http.createServer((req, res) => {
+//     res.end('Response from the server');
+// });
+// server.listen(3000, () => {
+//     console.log(`Server was started on Port: 3000`);
+// })
 
