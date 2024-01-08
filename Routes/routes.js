0926633a8628b128fb009658d17af814e41ac2c3
Am1d1v <dimadiman1adima@gmail.com>
2024-01-08 16:12:06 +0300

    .

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,17 +3,24 @@ const express = require('express');
 const PORT = 5000;
 const app = express();
 
-const firstHandler = (req, res, next) => {
-    console.log("Handler");
-    next();
+// Get root route
+const getRootHandler = (req, res, next) => {
+    res.send('Get root route');
 }
 
-const secondHandler = (req, res) => {
-    res.send('<h1>Greetings</h1>');
+// Get Comments route
+const getCommentsHandler = (req, res) => {
+    res.send('Get Comments route')
 }
 
+// Post comments route
+const postCommentsHandler = (req, res) => {
+    res.send('Post comments route')
+}
 
-app.get('/', firstHandler, secondHandler);
+app.get('/', getRootHandler);
+app.get('/comments', getCommentsHandler);
+app.post('/comments', postCommentsHandler)
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
